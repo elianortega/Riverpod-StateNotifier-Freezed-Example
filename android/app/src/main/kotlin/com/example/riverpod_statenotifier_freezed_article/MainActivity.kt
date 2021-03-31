6704f0b6ebffe3b1b10e17e9e0b1e398d0f65821
Elian Ortega <elian.mortega@gmail.com>
2021-03-31 07:19:44 -0600

    First Commit Example App

---
+++ android/app/src/main/kotlin/com/example/riverpod_statenotifier_freezed_article/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.riverpod_statenotifier_freezed_article
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:riverpod_statenotifier_freezed_article/src/app.dart';
+
+void main() {
+  runApp(MyApp());
+}
